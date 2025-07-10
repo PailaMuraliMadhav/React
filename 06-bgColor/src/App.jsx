6a5173c563c711be9ee40867523e92922595113b
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-07-10 10:31:20 +0530

    done

--- 06-bgColor/src/App.jsx
+++ 06-bgColor/src/App.jsx
@@ -1,44 +1,43 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [color, setColor] = useState("pink");
 
   return (
     <div
-      className="w-full h-screen duration-300 transition-colors"
+      className="w-screen h-screen duration-300 transition-colors"
       style={{ backgroundColor: color }}
     >
-      <div className="fixed flex justify-center px-4 bottom-12">
+      <div className="fixed flex justify-center bottom-12 px-4 w-full">
         <div className="flex gap-4 bg-white shadow-2xl px-3 py-3 rounded-2xl border border-gray-200">
           <button
             onClick={() => setColor("red")}
-            style={{ backgroundColor: "red" }}
             className="px-5 py-2 rounded-full text-white font-semibold shadow-md transition transform active:scale-95"
+            style={{ backgroundColor: "red" }}
           >
             Red
           </button>
 
           <button
             onClick={() => setColor("blue")}
-            style={{ backgroundColor: "blue" }}
             className="px-5 py-2 rounded-full text-white font-semibold shadow-md transition transform active:scale-95"
+            style={{ backgroundColor: "blue" }}
           >
             Blue
           </button>
 
           <button
             onClick={() => setColor("green")}
-            style={{ backgroundColor: "green" }}
             className="px-5 py-2 rounded-full text-white font-semibold shadow-md transition transform active:scale-95"
+            style={{ backgroundColor: "green" }}
           >
             Green
           </button>
 
           <button
             onClick={() => setColor("orange")}
-            style={{ backgroundColor: "orange" }}
             className="px-5 py-2 rounded-full text-white font-semibold shadow-md transition transform active:scale-95"
+            style={{ backgroundColor: "orange" }}
           >
             Orange
           </button>
